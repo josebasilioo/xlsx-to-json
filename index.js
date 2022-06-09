3e6b2be7f7a76b50f60fcbabb35445b00948234f
josebasilioo <josebasiliosilvaneto@gmail.com>
2022-06-08 23:09:22 -0300

    test read xls file

---
+++ index.js
@@ -0,0 +1,14 @@
+import * as XLSX from 'xlsx';
+import * as fs from 'fs';
+// import * as fs from 'fs';
+
+// XLSX.set_fs(fs);
+let workSheets = {}
+const workbook = XLSX.read(fs.readFileSync('./sheet-prices.xls'));
+for (const sheetName of workbook.SheetNames) {
+    workSheets[sheetName] = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName]);
+    console.log(workSheets[sheetName]);
+}
+
+
+console.log(Object.entries(workSheets));
